4afdab7d8e63a20d0ddea4fe38300e49e534777e
LovisottoSantiago <santiagolovisotto@gmail.com>
2024-08-12 18:25:01 -0300

    Initial commit

---
+++ src/main/java/com/stoicpath/TheStoicPath/TheStoicPathApplication.java
@@ -0,0 +1,13 @@
+package com.stoicpath.TheStoicPath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheStoicPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheStoicPathApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stoicpath/TheStoicPath/TheStoicPathApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoicpath.TheStoicPath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheStoicPathApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
